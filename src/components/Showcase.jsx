53c2a683bbffd24489a40909a31731a2576c9f96
mustabur <mustaburrahman2017@hotmail.com>
2020-07-04 17:07:03 +0100

    added portait image to personal website

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Showcase() {
     return (
         <div className="showcase">
+            <img className="portrait-pic" src={require('../portrait.jpeg')} alt="portrait"/>
            <h1>Mustabur Rahman</h1>
            <p>Junior Software Developer</p>
            <p>Wesbite build from the ground up using React.js framework</p>
