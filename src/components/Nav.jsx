624b3af7332a1a23ff69740098b66873b3cc2bb1
mustabur <mustaburrahman2017@hotmail.com>
2020-07-18 17:10:30 +0100

    adding responsiveness to the navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from '@reach/router'
+import MobileNav from './mobileNav'
 
 function Nav() {
     return (
@@ -16,6 +17,7 @@ function Nav() {
             <li>Contact</li>
             </Link>
             </ul>
+            <MobileNav />
         </nav>
     )
 }
---
+++ src/components/mobileNav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from '@reach/router'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faTwitter, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
+
+function MobileNav() {
+    return (
+        <nav className="mobile-nav">
+            <ul>
+            <Link to={"/"}>
+            <FontAwesomeIcon className="twitter-icon" icon={faTwitter}/>
+            </Link>
+            <Link to={"/projects"}>
+            <FontAwesomeIcon className="github-icon" icon={faGithub}/>
+            </Link>
+            <Link to={"/contact"}>
+            <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin} />
+            </Link>
+            </ul>
+        </nav>
+    )
+}
+
+export default MobileNav
