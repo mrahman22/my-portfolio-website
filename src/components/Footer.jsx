b18e47c44cd73a63d3119109851c6442d66067c1
mustabur <mustaburrahman2017@hotmail.com>
2020-08-11 21:05:17 +0100

    added links to linked website and github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {Link} from "@reach/router";
 import {faTwitter, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 library.add(faTwitter, faGithub, faLinkedin)
@@ -10,8 +11,12 @@ function Footer() {
         <div className="footer-container">
         <footer className="footer">
             {/* <FontAwesomeIcon className="twitter-icon" icon={faTwitter}/> */}
+            <a href="https://github.com/mrahman22">
             <FontAwesomeIcon className="github-icon" icon={faGithub}/>
+            </a>
+            <a href="https://www.linkedin.com/in/mustabur-rahman-7460981aa/">
             <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin} />
+            </a>
         </footer>
         </div>
     )
