17d9cd9d01c42f91d358f1f1aa1eab5540d8bbf7
mustabur <mustaburrahman2017@hotmail.com>
2021-01-18 17:01:25 +0000

    typewriter effect added to showcase section

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from "react";
+import Typewriter from "typewriter-effect";
 
 function Showcase() {
-    return (
-        <div className="showcase">
-            <img className="portrait-pic" src={require('../portrait.jpeg')} alt="portrait"/>
-           <h1>Mustabur Rahman</h1>
-           <p>Junior Software Developer</p>
-           <p>Website built from the ground up using React.js framework</p>
-        </div>
-    )
+  return (
+    <div className="showcase">
+      <img
+        className="portrait-pic"
+        src={require("../portrait.jpeg")}
+        alt="portrait"
+      />
+      <h1>Mustabur Rahman</h1>
+      <Typewriter
+        options={{
+          strings: [
+            "Junior software developer",
+            "Website built from the ground up using React.js framework",
+          ],
+          autoStart: true,
+          loop: true,
+
+        }}
+      />
+    </div>
+  );
 }
 
-export default Showcase
+export default Showcase;
